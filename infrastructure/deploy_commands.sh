6988c3b5df4fdaa1268522a7e86b4270bec384c5
omerhac <omerhac94@gmail.com>
2021-05-01 20:39:20 +0300

    added cloudformation template for deploying order processing infrastructure

---
+++ infrastructure/deploy_commands.sh
@@ -0,0 +1,15 @@
+### file for packaging and deploying the project infrastructure on cloudformation
+
+# package voice recognition stack
+aws cloudformation package --s3-bucket project-shoplift --template-file infrastructure/voice_recognition.yaml --output-template-file infrastructure/gen/voice_recognition.yaml
+
+# deploy voice recognition stack
+aws cloudformation deploy --template-file /Users/home/PycharmProjects/shoplift/infrastructure/gen/voice_recognition.yaml --stack-name ShopliftVoiceRecognition \
+ --capabilities CAPABILITY_NAMED_IAM
+
+# package order processing stack
+aws cloudformation package --s3-bucket project-shoplift --template-file infrastructure/order_proccess.yaml --output-template-file infrastructure/gen/order_proccess.yaml
+
+# deploy order processing stack
+aws cloudformation deploy --template-file /Users/home/PycharmProjects/shoplift/infrastructure/gen/order_proccess.yaml --stack-name ShopliftOrderProcess \
+ --capabilities CAPABILITY_NAMED_IAM
